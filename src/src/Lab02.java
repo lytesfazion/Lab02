2e816a54df80125e3c799fe3634ba1d53589ac26
Liwam <liwamtesfazion@gmail.com>
2021-02-16 22:00:18 -0800

    lab01 commit

--- src/src/Lab02.java
+++ src/src/Lab02.java
@@ -8,28 +8,33 @@ public class Lab02 {
         int []noSumArray = new int[1];
         // this is to make sure only one of the above arrays are returned.
         Boolean returnStatement = null;
-        //this is to make sure I catch the sum and be able to compare it in the if statement(line 32) bc tempTotal is
+        //this is to make sure I catch the sum and be able to compare it in the if statement(line 32)
+        // bc tempTotal is going to continue to grow after I find the numbers that get the sum.
+
         int targetSumTotal = 0;
 
         for (int i=0; i<num.length; i++) {
             for (int j = i + 1; j < num.length; j++) {
                 int tempTotal = num[i] + num[j];
-                System.out.println(tempTotal);
+                //System.out.println(tempTotal);
 
                 if (tempTotal == sum) {
                     sumIndex[0] = i;
                     sumIndex[1] = j;
+                    //if tempTotal == sum then I've found the indices.
+                    //So I want to save tempTotal in my new variable targetSumTotal.
+                    //
                     targetSumTotal = tempTotal;
                     returnStatement = true;
                 }
             }
-
         }
+        //this is where I check, after I've gone through the whole array, if I should return -1
         if (targetSumTotal != sum) {
             noSumArray[0] = -1;
             returnStatement = false;
         }
-
+        //this is where my boolean comes into play, so I can only return the correct array.
         if (returnStatement == true){
             return sumIndex;
         }else {
@@ -37,16 +42,17 @@ public class Lab02 {
         }
     }
     public static void main(String[] args) {
-        int[] numarray = {2,12,8,7} ;
-        //int[] numarray = {1,13,18,20,30} ;
-        int targetsum = 9;
-        //int targetsum = 40;
+        //int[] numarray = {2,12,8,7} ;
+        int[] numarray = {1,13,18,20,30} ;
+        //int targetsum = 9;
+        int targetsum = 40;
         Lab02 function = new Lab02();
         System.out.println(Arrays.toString(function.twosum(numarray, targetsum)));
 
     }
 
     /**
+     * I don't know if this was asked for
      * Analysis:
      * runtime: n^2
      * space : n (linear)
